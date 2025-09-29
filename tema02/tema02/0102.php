7aa6c7825f4121d7a9d954472d0a4aee06a1f3c9
Ahabd4711 <af8240892@gmail.com>
2025-09-29 14:20:43 +0200

    tema01

---
+++ tema02/tema02/0102.php
@@ -0,0 +1,22 @@
+<?php
+/*
+/*
+/*
+*/
+$svar=$_POST ["svar"];
+
+if ($svar == 9) 
+    {
+        print("riktig. 3 ganger 3 er 9");
+    }
+    else
+    {
+        print("Feil. 3 ganger er ikke $svar. 3 ganger 3 er 9");
+        }
+    ?>
+    
+    
+        
+    
+
+    
