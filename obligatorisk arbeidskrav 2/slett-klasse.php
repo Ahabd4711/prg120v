7426ae6b23d66f418eec345856c3f827090ceaf8
Ahabd4711 <af8240892@gmail.com>
2025-10-27 14:03:11 +0100

    haha

--- obligatorisk arbeidskrav 2/slett-klasse.php
+++ obligatorisk arbeidskrav 2/slett-klasse.php
@@ -1,18 +1,18 @@
-<?php /* slett-studium */
+<?php /* slett-klasse */
 /*
-/* Programmet lager et skjema for å kunne slette et studium
-/* Programmet sletter det valgte studiet
+/* Programmet lager et skjema for å kunne slette en klasse
+/* Programmet sletter den valgte klassen
 */
 ?>
 <script src="funksjoner.js"> </script>
-<h3>Slett studium</h3>
+<h3>Slett klasse</h3>
 <form method="post" action="" id="slettStudiumSkjema" name="slettStudiumSkjema" onSubmit="return bekreft()">
-Studium
+Klasse
 <select name="studiumkode" id="studiumkode">
-<?php print("<option value=''>velg studium </option>");
+<?php print("<option value=''>velg klasse </option>");
 include("dynamiske-funksjoner.php"); listeboksStudiumkode(); ?>
 </select> <br/>
-<input type="submit" value="Slett studium" name="slettStudiumKnapp" id="slettStudiumKnapp" />
+<input type="submit" value="Slett klasse" name="slettStudiumKnapp" id="slettStudiumKnapp" />
 </form>
 <?php
 if (isset($_POST ["slettStudiumKnapp"]))
@@ -20,7 +20,7 @@ if (isset($_POST ["slettStudiumKnapp"]))
 $studiumkode=$_POST ["studiumkode"];
 if (!$studiumkode)
 {
-print ("Det er ikke valgt noe studium");
+print ("Det er ikke valgt noe klasse");
 }
 else
 {
@@ -28,7 +28,9 @@ include("db-tilkobling.php"); /* tilkobling til database-serveren utført og val
 $sqlSetning="DELETE FROM studium WHERE studiumkode='$studiumkode';";
 mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
 /* SQL-setning sendt til database-serveren */
-print ("F&oslash;lgende studium er n&aring; slettet: $studiumkode <br />");
+print ("F&oslash;lgende klasse er n&aring; slettet: $studiumkode <br />");
 }
 }
 ?>
+
+
